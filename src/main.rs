023eeccae5801d9daff11fb2901ac985e5ccca0e
morayo0x00 <morayo0x00@gmail.com>
2024-08-23 14:36:45 +0100

    project skeleton setup acheived.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-//! main.rs
-use std::net::TcpListener;
-
-use zero2prod::run;
-
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind to random port");
-    run(listener)?.await
+fn main() {
+    println!("Hello World");
 }
